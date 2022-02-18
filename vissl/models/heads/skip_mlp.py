ae84eca6e444c1766fd60454c60967eeb85541ce
clane9 <connor.lane858@gmail.com>
2022-02-18 11:26:08 -0500

    add skip_mlp head to be used as prediction head

---
+++ vissl/models/heads/skip_mlp.py
@@ -0,0 +1,37 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from typing import Tuple, Union
+
+import torch
+from vissl.models.heads import MLP, register_model_head
+
+
+@register_model_head("skip_mlp")
+class SkipMLP(MLP):
+    """
+    An MLP that also returns its input. Useful for constructing prediction heads
+    on top of projection heads, as used in BYOL, SimSiam, NNCLR, etc.
+
+    NOTE: The only planned use case is as a final prediction head. Can we imagine
+    any use cases as an intermediate head module?
+    """
+
+    def forward(
+        self, batch: Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Args:
+            batch: 2D torch tensor or 4D tensor of shape `N x C x 1 x 1`, or
+                tuple of such tensors
+
+        Returns:
+            out: 2D output torch tensor
+            batch: 2D input torch tensor
+        """
+        if isinstance(batch, tuple):
+            batch = batch[0]
+        out = super(SkipMLP, self).forward(batch)
+        return out, batch
