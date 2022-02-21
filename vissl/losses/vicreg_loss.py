321812d8148db5f2da1960db42a28dfe4ce1f940
clane9 <connor.lane858@gmail.com>
2022-02-21 08:44:48 -0500

    update vicreg loss for distributed training

--- vissl/losses/vicreg_loss.py
+++ vissl/losses/vicreg_loss.py
@@ -9,6 +9,7 @@ import torch
 from classy_vision.losses import ClassyLoss, register_loss
 from torch.nn import functional as F
 from vissl.config import AttrDict
+from vissl.utils.distributed_utils import gather_from_all
 
 
 @register_loss("vicreg_loss")
@@ -39,41 +40,60 @@ class VICRegLoss(ClassyLoss):
         # copied verbatim
         z1, z2 = torch.chunk(output, 2)
 
+        # global mean and variance across replicas, similar to SyncBatchNorm
+        # TODO: check carefully
+        m1, v1 = sync_mean_var(z1)
+        m2, v2 = sync_mean_var(z2)
+
         # invariance + variance + covariance loss
-        # TODO: var loss is not strictly separable across the batch, due to the
-        # sqrt. Also the batch mean used to center should probably me shared.
-        # Think/check what to do in distributed setting.
-        # Should we all reduce the batch mean and variance to get it correct?
         loss = (
             self.loss_config.lambd * mse_loss(z1, z2)
-            + self.loss_config.mu * (var_loss(z1) + var_loss(z2))
-            + self.loss_config.nu * (cov_loss(z1) + cov_loss(z2))
+            + self.loss_config.mu * (var_loss(v1) + var_loss(v2))
+            + self.loss_config.nu * (cov_loss(z1, m1) + cov_loss(z2, m2))
         )
         return loss
 
     def __repr__(self):
         repr_dict = {
             "name": self._get_name(),
-            "lambd": self.loss_config.lambd,
+            "lambda_": self.loss_config.lambda_,
             "mu": self.loss_config.mu,
             "nu": self.loss_config.nu,
         }
         return pprint.pformat(repr_dict, indent=2)
 
 
-def mse_loss(z1, z2):
+def sync_mean_var(z: torch.Tensor):
+    # synchronized mean, var across replicas with grads
+    # NOTE: assuming all replica batch sizes equal
+    mean = z.mean(dim=0).unsqueeze(0)
+    means = gather_from_all(mean)
+    mean = means.mean(dim=0)
+
+    batch_size = z.shape[0]
+    world_size = means.shape[0]
+    effective_batch_size = world_size * batch_size
+
+    var = (z - mean).pow(2).sum(dim=0)
+    vars = gather_from_all(var)
+    var = vars.sum(dim=0).div(effective_batch_size - 1)
+    return mean, var
+
+
+def mse_loss(z1: torch.Tensor, z2: torch.Tensor):
     return (z1 - z2).pow(2).sum(dim=1).mean()
 
 
-def var_loss(z: torch.Tensor):
-    # NOTE: not batch separable. It it supposed to be modified in some way?
-    std = torch.sqrt(z.var(dim=0) + 1e-4)
+def var_loss(var: torch.Tensor):
+    std = torch.sqrt(var + 1e-4)
     return F.relu(1 - std).mean()
 
 
-def cov_loss(z: torch.Tensor):
+def cov_loss(z: torch.Tensor, mean: torch.Tensor):
     n, d = z.shape
-    z = z - z.mean(dim=0)
+    z = z - mean
+    # TODO: don't think we need to sync cov since the loss is batch separable
+    # but check to be sure
     cov = (z.T @ z) / (n - 1)
     cov = cov - cov.diag().diag()
     return cov.pow(2).sum().div(d)
